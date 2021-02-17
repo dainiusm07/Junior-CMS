fa29debdf6f7c4d1500371998bfcf39d4130bca3
dainiusm07 <dainius.masevicius@gmail.com>
2021-02-17 09:09:05 +0200

    (test): Removing comment after everything got fixed

--- packages/core/src/service/services/base.service.spec.ts
+++ packages/core/src/service/services/base.service.spec.ts
@@ -5,7 +5,6 @@ import { getRepositoryToken } from "@nestjs/typeorm";
 import { BaseEntity } from "../../entities/base/base.entity";
 
 const updateResults: UpdateResult[] = [
-  // test push
   { affected: 1, generatedMaps: [], raw: [] },
   { affected: 0, generatedMaps: [], raw: [] },
 ];
